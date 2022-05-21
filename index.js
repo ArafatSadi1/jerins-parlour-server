90d009d7e71ac6be905f7b070e9db71f721d7c47
ArafatSadi1 <arafatsadi1@gmail.com>
2022-05-21 20:08:54 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,199 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000;
+const nodemailer = require("nodemailer");
+const sgTransport = require("nodemailer-sendgrid-transport");
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fp0v7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+
+const verifyJwt = async(req, res, next) =>{
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'Unauthorized Access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if(err){
+            return res.status(403).send({message: 'Forbidden Access'})
+        }
+        req.decoded = decoded;
+        next()
+    });
+};
+
+
+var emailSenderOptions = {
+  auth: {
+    api_key: process.env.EMAIL_SENDER_KEY,
+  },
+};
+
+const emailClient = nodemailer.createTransport(sgTransport(emailSenderOptions));
+ 
+function sendBookingEmail(booking) {
+    const {email, name} = booking;
+  const sendEmail = {
+    from: process.env.EMAIL_SENDER,
+    to: email,
+    subject: `Jerins parlour booking for ${name}`,
+    text: `Hello, We have recieve your booking ${name}`,
+    html: `
+    <div>
+        <h2>Hello</h2>
+        <p>Thanks For Your booking</p>
+        <p>We have recieve your booking ${name}</p>
+        <h3>Our Address</h3>
+        <p>barishal, bangladesh</p>
+    </div>
+    `,
+  };
+
+  emailClient.sendMail(sendEmail, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Message sent: ", info);
+    }
+  });
+};
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client
+      .db("jerins-parlour")
+      .collection("services");
+    const bookingCollection = client.db("jerins-parlour").collection("booking");
+    const userCollection = client.db("jerins-parlour").collection("user");
+    const paymentCollection = client.db("jerins-parlour").collection("payment");
+
+    const verifyAdmin = async(req, res, next) =>{
+      // const requester = req.decoded.email;
+      console.log(req)
+      // console.log(requester)
+      next()
+    }    
+
+    app.post('/create-payment-intent', async(req, res)=>{
+        const {price} = req.body;
+        const amount = price * 100;
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: 'usd',
+            payment_method_types: ['card']
+        })
+        res.send({clientSecret: paymentIntent.client_secret})
+    })
+
+    app.patch('/booking/:id', async(req, res)=>{
+        const id = req.params.id;
+        const booking = req.body;
+        const {bookingId, transactionId} = booking;
+        const filter = {_id: ObjectId(id)};
+        const updatedDoc = {
+            $set: {
+                paid: true,
+                transactionId: transactionId
+            }
+        }
+        const result = await paymentCollection.insertOne({bookingId, transactionId});
+        const updatedBooking = await bookingCollection.updateOne(filter, updatedDoc);
+        res.send(updatedBooking);
+    })
+
+    app.post("/booking", async (req, res) => {
+        const booking = req.body;
+        const result = await bookingCollection.insertOne(booking);
+        console.log('Email sent')
+      //   sendBookingEmail(booking);
+        res.send(result);
+      });
+
+      app.get('/booking', async(req, res)=>{
+          const result = await bookingCollection.find({}).toArray();
+          res.send(result)
+      })
+  
+      app.get("/booking/:email", verifyJwt, async (req, res) => {
+        const email = req.params.email;
+        const filter = { email: email};
+        const result = await bookingCollection.find(filter).toArray();
+        res.send(result);
+      });  
+
+    app.put('/user/:email', async(req, res)=>{
+        const email = req.params.email;
+        const user = req.body;
+        const filter = {email: email};
+        const options = { upsert: true };
+        const updatedDoc = {
+            $set: user,
+        }
+        const result = await userCollection.updateOne(filter, updatedDoc, options);
+        const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'});
+        res.send({result, token})
+    })
+
+    app.get('/admin/:email', verifyJwt, async(req, res)=>{
+      const email = req.params.email;
+      const user = await userCollection.findOne({email: email})
+      const isAdmin = user.role === "admin";
+      res.send({admin: isAdmin})
+    })
+
+    app.patch('/user/admin/:email', async(req, res)=>{
+        const email = req.params.email;
+        const filter = {email: email};
+        const updatedDoc = {
+            $set: { role: "admin" }
+        }
+        const result = await userCollection.updateOne(filter, updatedDoc)
+        res.send(result)
+    })
+
+    app.get("/services", async (req, res) => {
+      const result = await serviceCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    app.post('/services', verifyJwt, async(req, res)=>{
+        const service = req.body;
+        const result = await serviceCollection.insertOne(service);
+        res.send(result)
+    })
+
+    app.get("/payment/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id)};
+      const result = await bookingCollection.findOne(filter);
+      res.send(result);
+    });
+
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello jerins parlour server");
+});
+
+app.listen(port, () => {
+  console.log("jerins parlour server is running from", port);
+});
