368b66e6ba5ab585c0e849f52ba5e1110eb5920b
ArafatSadi1 <arafatsadi1@gmail.com>
2022-09-17 20:03:38 +0600

    some update

--- index.js
+++ index.js
@@ -166,10 +166,10 @@ async function run() {
       res.send({ result, token });
     });
 
-    app.get("/admin/:email", verifyJwt, async (req, res) => {
+    app.get("/admin/:email", async (req, res) => {
       const email = req.params.email;
       const user = await userCollection.findOne({ email: email });
-      const isAdmin = user.role === "admin";
+      const isAdmin = user?.role === "admin";
       res.send({ admin: isAdmin });
     });
 
